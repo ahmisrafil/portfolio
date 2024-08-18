0559ab0881b60d725c8b5ede7e89522efb1ca790
ahmisrafil <ahmisrafil@yahoo.com>
2024-08-19 02:16:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h3 className='text-center text-4xl text-orange-400'>Welcome to portfolio website</h3>
+    </div>
+      
+  )
+}
+
+export default App

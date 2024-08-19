c60f461d5c1cc2409d0f27ef9045ac1464ac38f5
ahmisrafil <ahmisrafil@yahoo.com>
2024-08-20 02:36:27 +0600

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    const navLinks = <>
+        <li className="lg:text-white lg:text-xl lg:font-sans  lg:mx-2">
+            <NavLink to='/'>Home</NavLink>
+        </li>
+        <li className="lg:text-white lg:text-xl lg:font-sans  lg:mx-2">
+            <NavLink to='/'>Add Product</NavLink>
+        </li>
+        <li className="lg:text-white lg:text-xl lg:font-sans  lg:mx-2">
+            <NavLink to='/'>My Cart</NavLink>
+        </li>
+       
+
+
+    </>
+    return (
+        <div className="navbar flex flex-col md:flex-row gap-4 bg-[#060d5e]  px-10">
+        <div className="navbar-start">
+            <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost text-white lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="flex gap-4 h-20">
+                    <img src="/src/assets/Israfil.png" alt="" />
+            </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal  px-1">
+                {navLinks}
+            </ul>
+        </div>
+    </div>
+    );
+};
+
+export default Navbar;

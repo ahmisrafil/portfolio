49adc0b2df049fb20e5b19c4e572142db0cf4497
ahmisrafil <ahmisrafil@yahoo.com>
2024-08-23 01:13:44 +0600

    slider text added to banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { FaGithub, FaLinkedin, FaSquareFacebook, FaSquareGithub } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom';
 import Typed from 'typed.js';
+import {motion} from 'framer-motion'
+
+const sliderVariants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: "-200%",
+      transition: {
+        repeat: Infinity,
+        repeatType:"mirror",
+        duration: 20,
+      },
+    },
+  };
 const Banner = () => {
     // Create reference to store the DOM element containing the animation
     const el = React.useRef(null);
@@ -21,26 +36,27 @@ const Banner = () => {
         };
     }, []);
     return (
-        <div className='flex flex-col lg:flex-row  justify-between lg:ml-20  items-center'>
+        <>
+        <div className='flex flex-col lg:flex-row  justify-between lg:ml-20 mb-60 items-center'>
             <div data-aos="fade-up-right">
                 <div>
                     <h3 className='text-xl font-bold ml-4'>Hey I'm,</h3>
-                    <h3 className='text-9xl font-extrabold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>AHM ISRAFIL</h3>
+                    <h3 className='lg:text-9xl font-extrabold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>AHM ISRAFIL</h3>
                 </div>
-                <div className="App text-4xl font-semibold text-amber-600 flex">
+                <div className="App lg:text-4xl font-semibold text-amber-600 flex">
                     <h3>I'm a-   </h3>
                     <span ref={el} />
                 </div>
                 <div className='flex flex-row gap-4 mt-6'>
-                <div>
-                    <NavLink to='https://www.linkedin.com/in/ahmisrafil' target='_blank'><FaLinkedin /></NavLink>
-                </div>
-                <div>
-                    <NavLink to='https://www.facebook.com/ahm.israfil' target='_blank'><FaSquareFacebook /></NavLink>
-                </div>
-                <div>
-                    <NavLink to='https://www.github.com/ahmisrafil' target='_blank'><FaSquareGithub /></NavLink>
-                </div>
+                    <div>
+                        <NavLink to='https://www.linkedin.com/in/ahmisrafil' target='_blank'><FaLinkedin /></NavLink>
+                    </div>
+                    <div>
+                        <NavLink to='https://www.facebook.com/ahm.israfil' target='_blank'><FaSquareFacebook /></NavLink>
+                    </div>
+                    <div>
+                        <NavLink to='https://www.github.com/ahmisrafil' target='_blank'><FaSquareGithub /></NavLink>
+                    </div>
                 </div>
             </div>
             <div>
@@ -49,6 +65,15 @@ const Banner = () => {
 
             </div>
         </div>
+        <motion.div
+                className="absolute text-[40vh] -bottom-32 whitespace-nowrap text-[#ffffff09] w-[50%] font-bold"
+                variants={sliderVariants}
+                initial="initial"
+                animate="animate"
+            >
+                AHM ISRAFIL
+            </motion.div>
+    </>
     );
 };
 
